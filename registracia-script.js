47ce7a538b29a6b9012e6df87688d59295991237
zenskakonferenciabj <zenska.konferencia.bj@gmail.com>
2025-09-04 23:27:50 +0200

    Update registracia-script.js

--- registracia-script.js
+++ registracia-script.js
@@ -1,3 +1,4 @@
+<!-- uved url skriptu na excel s workshopami -->
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwVQSnH293jN9fDYJN54ZHFX35MB8T5RtMmot__Yx2FA0WHr4v0BADer8DMnWxBL38s7w/exec';
 const WS1 = document.getElementById('workshop1');
 const WS2 = document.getElementById('workshop2');
